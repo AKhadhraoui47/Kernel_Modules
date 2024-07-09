57f8d17c4f47cbb02c5ef121e72958e10ae39982
AKhadhraoui47 <bkhadhraoui870@gmail.com>
2024-07-09 14:13:58 +0100

    Hello World Module

---
+++ Hello_World/Hello_mod.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h> 
+#include <linux/printk.h> 
+
+static int __init hello_init(void)
+{
+    pr_info("Hello, world\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    pr_info("Goodbye, world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
---
+++ Hello_World/Hello_mod.mod
@@ -0,0 +1,2 @@
+/home/tsarbomba/Documents/STM32MPU_ws/STM32MP1-Ecosystem-v5.0.0/Kernel_Modules/Hello_World/Hello_mod.o
+
